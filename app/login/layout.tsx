db85fd95ab35f8afc5378cc758428c4645d1d3ce
xPHUM <phumsocool@gmail.com>
2025-06-06 15:10:41 +0700

    อปเดตโปรเจกตลาสด

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { IBM_Plex_Sans_Thai } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const ibmPlexThai = IBM_Plex_Sans_Thai({ subsets: ['thai', 'latin'], weight: ['400', '700'] })
 
 export const metadata: Metadata = {
   title: 'เข้าสู่ระบบ...',
@@ -14,8 +14,8 @@ export default function LoginLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="th">
+      <body className={ibmPlexThai.className}>{children}</body>
     </html>
   )
 }
