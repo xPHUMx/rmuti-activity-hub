f028a9809a0ee61aff2325d681fbc6deecc41d7b
xPHUMx <phumsocool@gmail.com>
2026-07-16 10:36:17 +0700

    chore: hide chatbot component in layout

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -18,7 +18,7 @@ export default function ClientLayout({
       {pathname !== "/login" && (
         <>
           <VersionBadge />
-          <Chatbot />
+          {/* <Chatbot /> */}
         </>
       )}
       <AdminButton />
